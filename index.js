66870d25ccc0438d86320070643c3b8b545fc2df
Ryan Johnson <ryan@258webco.com>
2017-12-04 20:33:27 -0600

    Update manifest with staging

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 import compression from "compression";
 import express from "express";
+import fs from "fs";
+import request from "request";
 
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(compression());
+
+let keys;
+if (fs.existsSync("./keys.json")) {
+  keys = require("./keys.json");
+} else {
+  keys = JSON.parse(process.env.VCAP_SERVICES)["user-provided"][0].credentials;
+}
+app.get("/github", (req, res) => {
+  request(
+    {
+      url: `https://api.github.com/user/repos?affiliation=owner,collaborator&access_token=${
+        keys.github
+      }`,
+      headers: {
+        "user-agent": "node.js"
+      }
+    },
+    (err, response, body) => {
+      if (!err && response.statusCode === 200) {
+        res.send(body);
+      }
+    }
+  );
+});
 
 app.use(express.static("./build"));
 
